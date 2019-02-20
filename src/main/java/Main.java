a531dbca850384d1464b2f41400cc01c374c968b
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-02-20 13:52:07 +0530

    Add image base64 support

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,9 +6,9 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.awt.image.RenderedImage;
+import java.io.*;
+import java.util.Base64;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
@@ -17,6 +17,7 @@ public class Main {
     WebElement elementCategory = null;
     String reportPath = args[0];
     String destinationPath = args[1];
+    String image = "";
     try {
       if (!new File(reportPath).exists()) {
         throw new Exception("Report not found at " + reportPath);
@@ -45,14 +46,15 @@ public class Main {
       }
 
       if(elementCategory != null)
-        takeSnapShot(driver, destinationPath + "categoryImage.png", elementCategory);
+        image = takeSnapShot(driver, destinationPath + "categoryImage.png", elementCategory);
 
       System.out.println("Creating HTML file...");
       File resultFile = new File(destinationPath + "emailReport.html");
       PrintWriter writer = new PrintWriter(resultFile);
       writer.write("<html>\n");
       writer.append("<body>\n");
-      writer.append("<p><img src=\"categoryImage.png\"/></p>\n");
+//      writer.append("<p><img data-inline=\"true\" src=\"categoryImage.png\"/></p>\n");
+      writer.append("<p><img alt=\"image\" data-inline=\"true\" src=\"data:image/png;base64, ").append(image).append("\"/></p>\n");
       writer.append("</html>\n");
       writer.close();
       System.out.println("Email Report Generation Complete!");
@@ -65,7 +67,8 @@ public class Main {
     }
   }
 
-  private static void takeSnapShot(WebDriver webdriver, String destinationPath, WebElement webElement) {
+  private static String takeSnapShot(WebDriver webdriver, String destinationPath, WebElement webElement) {
+    String base64String = "";
     try {
       TakesScreenshot scrShot = ((TakesScreenshot) webdriver);
       File srcFile = scrShot.getScreenshotAs(OutputType.FILE);                                      //storing screenshot in srcfile
@@ -75,13 +78,35 @@ public class Main {
       int width = webElement.getSize().getWidth();                                                 //find width from that corner
       int height = webElement.getSize().getHeight();                                              // find height from that corner
       BufferedImage ElementScreenshot = fullImg.getSubimage(point.getX(), point.getY(), width, height);//getting subimage from complete webpage
+      base64String = imgToBase64String(ElementScreenshot);
       ImageIO.write(ElementScreenshot, "png", srcFile);
-      File DestFile = new File(destinationPath);                                                       //create the png file in particular folder
+      File DestFile = new File(destinationPath);
+//      byte[] encodedBytes = Base64.getEncoder().encode("Test".getBytes());
+//      base64String = new String(encodedBytes);
+//      System.out.println("encodedBytes : " + base64String);
+//      System.out.println("Arrays.toString encodedBytes : " + Arrays.toString(encodedBytes));
+//      byte[] decodedBytes = Base64.getDecoder().decode(encodedBytes);
+//      System.out.println("decodedBytes " + new String(decodedBytes));
       FileUtils.copyFile(srcFile, DestFile);
     } catch (WebDriverException e) {
       System.out.println("webdriver Exception");
     } catch (IOException e) {
       System.out.println("InputOutput Exception");
+    }
+    return base64String;
+  }
+
+  private static String imgToBase64String(final RenderedImage img)
+  {
+    final ByteArrayOutputStream os = new ByteArrayOutputStream();
+    try
+    {
+      ImageIO.write(img, "png", os);
+      return Base64.getEncoder().encodeToString(os.toByteArray());
+    }
+    catch (final IOException ioe)
+    {
+      throw new UncheckedIOException(ioe);
     }
   }
 }
